06147741e962a98b6d9bbbe37e2e80ffffb36b82
Steve Bennett <steveb@workware.net.au>
2011-09-26 07:38:05 +1000

    Missing parens in non-utf8 version of utf8_strlen
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- utf8.h
+++ utf8.h
@@ -21,7 +21,7 @@ int utf8_fromunicode(char *p, unsigned short uc);
 #include <ctype.h>
 
 /* No utf-8 support. 1 byte = 1 char */
-#define utf8_strlen(S, B) (B) < 0 ? strlen(S) : (B)
+#define utf8_strlen(S, B) ((B) < 0 ? strlen(S) : (B))
 #define utf8_tounicode(S, CP) (*(CP) = (unsigned char)*(S), 1)
 #define utf8_upper(C) toupper(C)
 #define utf8_lower(C) tolower(C)
