msteveb/jimtcl

commit 7edde07972b9a43cd4e6305bd0e56b4a972ac8c3

Throw an error when parsing a bad script
If a script has a missing bracket (for example), currently
it is silently ignored, except by [source]

Signed-off-by: Steve Bennett <steveb@workware.net.au>

jim.c
@@ -1160,7 +1160,7 @@ void Jim_FreeStackElements(Jim_Stack *stack, void (*freeFunc) (void *ptr))
  * Results of missing quotes, braces, etc. from parsing.
  */
 struct JimParseMissing {
-    int ch;             /* At end of parse, ' ' if complete, '{' if braces incomplete, '"' if quotes incomplete */
+    int ch;             /* At end of parse, ' ' if complete or '{', '[', '"', '\\' , '{' if incomplete */
     int line;           /* Line number starting the missing token */
 };
 
@@ -3188,7 +3188,7 @@ static Jim_Obj *JimNewScriptLineObj(Jim_Interp *interp, int argc, int line)
  */
 static void FreeScriptInternalRep(Jim_Interp *interp, Jim_Obj *objPtr);
 static void DupScriptInternalRep(Jim_Interp *interp, Jim_Obj *srcPtr, Jim_Obj *dupPtr);
-static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr);
+static void JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr);
 static int JimParseCheckMissing(Jim_Interp *interp, int ch);
 
 static const Jim_ObjType scriptObjType = {
@@ -3288,7 +3288,8 @@ typedef struct ScriptObj
                                    only used by Jim_EvalObj() as protection against
                                    shimmering of the currently evaluated object. */
     int firstline;              /* Line number of the first line */
-    int linenr;                 /* Line number of the current line */
+    int linenr;                 /* Error line number, if any */
+    int missing;                /* Missing char if script failed to parse, or ' ' if OK */
 } ScriptObj;
 
 void FreeScriptInternalRep(Jim_Interp *interp, Jim_Obj *objPtr)
@@ -3623,7 +3624,7 @@ static void SubstObjAddTokens(Jim_Interp *interp, struct ScriptObj *script,
  * On parse error, sets an error message and returns JIM_ERR
  * (Note: the object is still converted to a script, even if an error occurs)
  */
-static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
+static void JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
 {
     int scriptTextLen;
     const char *scriptText = Jim_GetString(objPtr, &scriptTextLen);
@@ -3631,7 +3632,6 @@ static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
     struct ScriptObj *script;
     ParseTokenList tokenlist;
     int line = 1;
-    int retcode = JIM_OK;
 
     /* Try to get information about filename / line number */
     if (objPtr->typePtr == &sourceObjType) {
@@ -3648,8 +3648,6 @@ static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
             parser.tline);
     }
 
-    retcode = JimParseCheckMissing(interp, parser.missing.ch);
-
     /* Add a final EOF token */
     ScriptAddToken(&tokenlist, scriptText + scriptTextLen, 0, JIM_TT_EOF, 0);
 
@@ -3663,8 +3661,9 @@ static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
     else {
         script->fileNameObj = interp->emptyObj;
     }
-    script->linenr = parser.missing.line;
     Jim_IncrRefCount(script->fileNameObj);
+    script->missing = parser.missing.ch;
+    script->linenr = parser.missing.line;
 
     ScriptObjAddTokens(interp, script, &tokenlist);
 
@@ -3675,9 +3674,9 @@ static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
     Jim_FreeIntRep(interp, objPtr);
     Jim_SetIntRepPtr(objPtr, script);
     objPtr->typePtr = &scriptObjType;
-
-    return retcode;
 }
+
+static void JimAddErrorToStack(Jim_Interp *interp, int retcode, ScriptObj *script);
 
 /**
  * Returns NULL if the script failed to parse and leaves
@@ -3686,19 +3685,28 @@ static int JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
  * Otherwise returns a parsed script object.
  * (Note: the object is still converted to a script, even if an error occurs)
  */
-ScriptObj *Jim_GetScript(Jim_Interp *interp, Jim_Obj *objPtr)
+ScriptObj *Jim_GetScript(Jim_Interp *interp, Jim_Obj *objPtr, int flags)
 {
+    ScriptObj *script;
+
     if (objPtr == interp->emptyObj) {
         /* Avoid converting emptyObj to a script. use nullScriptObj instead. */
         objPtr = interp->nullScriptObj;
     }
 
     if (objPtr->typePtr != &scriptObjType || ((struct ScriptObj *)Jim_GetIntRepPtr(objPtr))->substFlags) {
-        if (JimSetScriptFromAny(interp, objPtr) == JIM_ERR) {
-            return NULL;
-        }
+        JimSetScriptFromAny(interp, objPtr);
     }
-    return (ScriptObj *) Jim_GetIntRepPtr(objPtr);
+
+    script = (ScriptObj *)Jim_GetIntRepPtr(objPtr);
+
+    if (JimParseCheckMissing(interp, script->missing)) {
+        if ((flags & JIM_ERRMSG)) {
+            JimAddErrorToStack(interp, JIM_ERR, script);
+        }
+        return NULL;
+    }
+    return script;
 }
 
 /* -----------------------------------------------------------------------------
@@ -10465,7 +10473,7 @@ int Jim_EvalObj(Jim_Interp *interp, Jim_Obj *scriptObjPtr)
     }
 
     Jim_IncrRefCount(scriptObjPtr);     /* Make sure it's shared. */
-    script = Jim_GetScript(interp, scriptObjPtr);
+    script = Jim_GetScript(interp, scriptObjPtr, JIM_ERRMSG);
     if (script == NULL) {
         Jim_DecrRefCount(interp, scriptObjPtr);
         return JIM_ERR;
@@ -10831,7 +10839,7 @@ static int JimCallProcedure(Jim_Interp *interp, Jim_Cmd *cmd, int argc, Jim_Obj 
     callFramePtr->staticVars = cmd->u.proc.staticVars;
 
     /* Remember where we were called from. */
-    script = Jim_GetScript(interp, interp->currentScriptObj);
+    script = Jim_GetScript(interp, interp->currentScriptObj, JIM_NONE);
     callFramePtr->fileNameObj = script->fileNameObj;
     callFramePtr->line = script->linenr;
 
@@ -11032,9 +11040,8 @@ int Jim_EvalFile(Jim_Interp *interp, const char *filename)
     Jim_IncrRefCount(scriptObjPtr);
 
     /* Now check the script for unmatched braces, etc. */
-    if (Jim_GetScript(interp, scriptObjPtr) == NULL) {
-        /* EvalFile changes context, so add a stack frame here */
-        JimAddErrorToStack(interp, JIM_ERR, (ScriptObj *)Jim_GetIntRepPtr(scriptObjPtr));
+    /* EvalFile changes context, so add a stack frame here */
+    if (Jim_GetScript(interp, scriptObjPtr, JIM_ERRMSG) == NULL) {
         Jim_DecrRefCount(interp, scriptObjPtr);
         return JIM_ERR;
     }
@@ -11646,7 +11653,7 @@ static int Jim_ForCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *argv
 
         /* Do it only if there aren't shared arguments */
         expr = JimGetExpression(interp, argv[2]);
-        incrScript = Jim_GetScript(interp, argv[3]);
+        incrScript = Jim_GetScript(interp, argv[3], JIM_NONE);
 
         /* Ensure proper lengths to start */
         if (incrScript == NULL || incrScript->len != 3 || !expr || expr->len != 3) {
@@ -12812,7 +12819,7 @@ static int Jim_DebugCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *ar
             Jim_WrongNumArgs(interp, 2, argv, "script");
             return JIM_ERR;
         }
-        script = Jim_GetScript(interp, argv[2]);
+        script = Jim_GetScript(interp, argv[2], JIM_NONE);
         if (script == NULL)
             return JIM_ERR;
         Jim_SetResultInt(interp, script->len);
@@ -14491,7 +14498,7 @@ static int Jim_InfoCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *arg
                 Jim_WrongNumArgs(interp, 2, argv, "");
                 return JIM_ERR;
             }
-            Jim_SetResult(interp, Jim_GetScript(interp, interp->currentScriptObj)->fileNameObj);
+            Jim_SetResult(interp, Jim_GetScript(interp, interp->currentScriptObj, JIM_NONE)->fileNameObj);
             break;
 
         case INFO_SOURCE:{
@@ -14516,7 +14523,7 @@ static int Jim_InfoCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *arg
                         line = argv[2]->internalRep.sourceValue.lineNumber;
                     }
                     else if (argv[2]->typePtr == &scriptObjType) {
-                        ScriptObj *script = Jim_GetScript(interp, argv[2]);
+                        ScriptObj *script = Jim_GetScript(interp, argv[2], JIM_NONE);
                         fileNameObj = script->fileNameObj;
                         line = script->firstline;
                     }

tests/errors.tcl
@@ -4,56 +4,60 @@ proc error_generator {type} {
 	switch $type {
 		badcmd {
 			bogus command called
-		} 
+		}
 		badvar {
 			set bogus
-		} 
+		}
 		error {
 			error bogus
-		} 
+		}
 		interpbadvar {
 			set x "some $bogus text"
-		} 
+		}
 		interpbadcmd {
 			set x "some $bogus text"
-		} 
+		}
 		package {
 			package require dummy
-		} 
+		}
 		source {
 			source dummy.tcl
-		} 
+		}
 		badpackage {
 			package require bogus
-		} 
+		}
 		returncode {
 			return -code error failure
-		} 
+		}
+		badproc {
+			error_badproc
+		}
 		default {
 			puts "Unknown type=$type"
 		}
 	}
 }
-
-
-
 # line 40: Some empty lines above so that line numbers don't change
 proc error_caller {type {method call}} {
 	switch $method {
 		call {
 			error_generator $type
-		} 
+		}
 		uplevel {
 			uplevel 1 [list error_generator $type]
-		} 
+		}
 		eval {
 			eval [list error_generator $type]
-		} 
+		}
 		evalstr {
 			eval error_generator $type
-		} 
+		}
 		default {
 			puts "Unknown method=$method"
 		}
 	}
+}
+
+proc error_badproc {} {
+	return [list missing bracket here
 }

tests/stacktrace.test
@@ -4,7 +4,7 @@ package require errors
 # Make this a proc so that the line numbers don't have to change
 proc main {} {
 	set id1 0
-	foreach type {badcmd badvar error interpbadvar interpbadcmd package source badpackage returncode} {
+	foreach type {badcmd badvar badproc error interpbadvar interpbadcmd package source badpackage returncode} {
 		set id2 0
 		incr id1
 		foreach method {call uplevel eval evalstr} {
@@ -74,38 +74,42 @@ set expected {
 	err-2.2 {1 {can't read "bogus": no such variable} {{} errors.tcl 9 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
 	err-2.3 {1 {can't read "bogus": no such variable} {{} errors.tcl 9 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
 	err-2.4 {1 {can't read "bogus": no such variable} {{} errors.tcl 9 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-3.1 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-3.2 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-3.3 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-3.4 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-4.1 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-4.2 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-4.3 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-4.4 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-5.1 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-5.2 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-5.3 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-5.4 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-6.1 {1 {from dummyproc
+	err-3.1 {1 {unmatched "["} {{} errors.tcl 62 error_badproc errors.tcl 33 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-3.2 {1 {unmatched "["} {{} errors.tcl 62 error_badproc errors.tcl 33 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-3.3 {1 {unmatched "["} {{} errors.tcl 62 error_badproc errors.tcl 33 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-3.4 {1 {unmatched "["} {{} errors.tcl 62 error_badproc errors.tcl 33 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-4.1 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-4.2 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-4.3 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-4.4 {1 bogus {{} errors.tcl 12 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-5.1 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-5.2 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-5.3 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-5.4 {1 {can't read "bogus": no such variable} {{} errors.tcl 15 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-6.1 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-6.2 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-6.3 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-6.4 {1 {can't read "bogus": no such variable} {{} errors.tcl 18 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-7.1 {1 {from dummyproc
 Can't load package dummy} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 21 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-6.2 {1 {from dummyproc
+	err-7.2 {1 {from dummyproc
 Can't load package dummy} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 21 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-6.3 {1 {from dummyproc
+	err-7.3 {1 {from dummyproc
 Can't load package dummy} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 21 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-6.4 {1 {from dummyproc
+	err-7.4 {1 {from dummyproc
 Can't load package dummy} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 21 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-7.1 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-7.2 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-7.3 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-7.4 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-8.1 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
-	err-8.2 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
-	err-8.3 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
-	err-8.4 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
-	err-9.1 {1 failure {{} errors.tcl 44 error_caller stacktrace.test 17}}
-	err-9.2 {1 failure {{} errors.tcl 47 error_caller stacktrace.test 17}}
-	err-9.3 {1 failure {{} errors.tcl 50 error_caller stacktrace.test 17}}
-	err-9.4 {1 failure {{} errors.tcl 53 error_caller stacktrace.test 17}}
+	err-8.1 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-8.2 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-8.3 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-8.4 {1 {from dummyproc} {{} dummy.tcl 3 dummyproc dummy.tcl 6 {} errors.tcl 24 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-9.1 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 44 error_caller stacktrace.test 17}}
+	err-9.2 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 47 error_caller stacktrace.test 17}}
+	err-9.3 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 50 error_caller stacktrace.test 17}}
+	err-9.4 {1 {Can't load package bogus} {{} errors.tcl 27 error_generator errors.tcl 53 error_caller stacktrace.test 17}}
+	err-10.1 {1 failure {{} errors.tcl 44 error_caller stacktrace.test 17}}
+	err-10.2 {1 failure {{} errors.tcl 47 error_caller stacktrace.test 17}}
+	err-10.3 {1 failure {{} errors.tcl 50 error_caller stacktrace.test 17}}
+	err-10.4 {1 failure {{} errors.tcl 53 error_caller stacktrace.test 17}}
 }
 
 # Set this to output expected results to stderr

commit 01f7cf2ef73d87e46edcfb3e561378551350db46

Improve performance of script validation commit
Commit 7edde0797 inadvertently reduced performance of the
script evaluation fast path.

Rename Jim_GetScript() to JimGetScript() - always returns a script, even on parse failure.
Now JimValidScript() checks the script for parse error and generates the error message if necessary.

Signed-off-by: Steve Bennett <steveb@workware.net.au>

jim.c
@@ -3289,7 +3289,7 @@ typedef struct ScriptObj
                                    shimmering of the currently evaluated object. */
     int firstline;              /* Line number of the first line */
     int linenr;                 /* Error line number, if any */
-    int missing;                /* Missing char if script failed to parse, or ' ' if OK */
+    int missing;                /* Missing char if script failed to parse, (or space or backslash if OK) */
 } ScriptObj;
 
 void FreeScriptInternalRep(Jim_Interp *interp, Jim_Obj *objPtr)
@@ -3676,19 +3676,15 @@ static void JimSetScriptFromAny(Jim_Interp *interp, struct Jim_Obj *objPtr)
     objPtr->typePtr = &scriptObjType;
 }
 
-static void JimAddErrorToStack(Jim_Interp *interp, int retcode, ScriptObj *script);
+static void JimAddErrorToStack(Jim_Interp *interp, ScriptObj *script);
 
 /**
- * Returns NULL if the script failed to parse and leaves
- * an error message in the interp result.
- *
- * Otherwise returns a parsed script object.
- * (Note: the object is still converted to a script, even if an error occurs)
+ * Returns the parsed script.
+ * Note that if there is any possibility that the script is not valid,
+ * call JimScriptValid() to check
  */
-ScriptObj *Jim_GetScript(Jim_Interp *interp, Jim_Obj *objPtr, int flags)
+ScriptObj *JimGetScript(Jim_Interp *interp, Jim_Obj *objPtr)
 {
-    ScriptObj *script;
-
     if (objPtr == interp->emptyObj) {
         /* Avoid converting emptyObj to a script. use nullScriptObj instead. */
         objPtr = interp->nullScriptObj;
@@ -3698,16 +3694,23 @@ ScriptObj *Jim_GetScript(Jim_Interp *interp, Jim_Obj *objPtr, int flags)
         JimSetScriptFromAny(interp, objPtr);
     }
 
-    script = (ScriptObj *)Jim_GetIntRepPtr(objPtr);
-
-    if (JimParseCheckMissing(interp, script->missing)) {
-        if ((flags & JIM_ERRMSG)) {
-            JimAddErrorToStack(interp, JIM_ERR, script);
-        }
-        return NULL;
-    }
-    return script;
+    return (ScriptObj *)Jim_GetIntRepPtr(objPtr);
 }
+
+/**
+ * Returns 1 if the script is valid (parsed ok), otherwise returns 0
+ * and leaves an error message in the interp result.
+ *
+ */
+static int JimScriptValid(Jim_Interp *interp, ScriptObj *script)
+{
+    if (JimParseCheckMissing(interp, script->missing) == JIM_ERR) {
+        JimAddErrorToStack(interp, script);
+        return 0;
+    }
+    return 1;
+}
+
 
 /* -----------------------------------------------------------------------------
  * Commands
@@ -10245,11 +10248,9 @@ int Jim_EvalObjPrefix(Jim_Interp *interp, Jim_Obj *prefix, int objc, Jim_Obj *co
     return ret;
 }
 
-static void JimAddErrorToStack(Jim_Interp *interp, int retcode, ScriptObj *script)
+static void JimAddErrorToStack(Jim_Interp *interp, ScriptObj *script)
 {
-    int rc = retcode;
-
-    if (rc == JIM_ERR && !interp->errorFlag) {
+    if (!interp->errorFlag) {
         /* This is the first error, so save the file/line information and reset the stack */
         interp->errorFlag = 1;
         Jim_IncrRefCount(script->fileNameObj);
@@ -10263,7 +10264,7 @@ static void JimAddErrorToStack(Jim_Interp *interp, int retcode, ScriptObj *scrip
     }
 
     /* Now if this is an "interesting" level, add it to the stack trace */
-    if (rc == JIM_ERR && interp->addStackTrace > 0) {
+    if (interp->addStackTrace > 0) {
         /* Add the stack info for the current level */
 
         JimAppendStackTrace(interp, Jim_String(interp->errorProc), script->fileNameObj, script->linenr);
@@ -10279,12 +10280,6 @@ static void JimAddErrorToStack(Jim_Interp *interp, int retcode, ScriptObj *scrip
         Jim_DecrRefCount(interp, interp->errorProc);
         interp->errorProc = interp->emptyObj;
         Jim_IncrRefCount(interp->errorProc);
-    }
-    else if (rc == JIM_RETURN && interp->returnCode == JIM_ERR) {
-        /* Propagate the addStackTrace value through 'return -code error' */
-    }
-    else {
-        interp->addStackTrace = 0;
     }
 }
 
@@ -10473,8 +10468,8 @@ int Jim_EvalObj(Jim_Interp *interp, Jim_Obj *scriptObjPtr)
     }
 
     Jim_IncrRefCount(scriptObjPtr);     /* Make sure it's shared. */
-    script = Jim_GetScript(interp, scriptObjPtr, JIM_ERRMSG);
-    if (script == NULL) {
+    script = JimGetScript(interp, scriptObjPtr);
+    if (!JimScriptValid(interp, script)) {
         Jim_DecrRefCount(interp, scriptObjPtr);
         return JIM_ERR;
     }
@@ -10671,7 +10666,14 @@ int Jim_EvalObj(Jim_Interp *interp, Jim_Obj *scriptObjPtr)
     }
 
     /* Possibly add to the error stack trace */
-    JimAddErrorToStack(interp, retcode, script);
+    if (retcode == JIM_ERR) {
+        JimAddErrorToStack(interp, script);
+    }
+    /* Propagate the addStackTrace value through 'return -code error' */
+    else if (retcode != JIM_RETURN || interp->returnCode != JIM_ERR) {
+        /* No need to add stack trace */
+        interp->addStackTrace = 0;
+    }
 
     /* Restore the current script */
     interp->currentScriptObj = prevScriptObj;
@@ -10839,7 +10841,7 @@ static int JimCallProcedure(Jim_Interp *interp, Jim_Cmd *cmd, int argc, Jim_Obj 
     callFramePtr->staticVars = cmd->u.proc.staticVars;
 
     /* Remember where we were called from. */
-    script = Jim_GetScript(interp, interp->currentScriptObj, JIM_NONE);
+    script = JimGetScript(interp, interp->currentScriptObj);
     callFramePtr->fileNameObj = script->fileNameObj;
     callFramePtr->line = script->linenr;
 
@@ -11038,13 +11040,6 @@ int Jim_EvalFile(Jim_Interp *interp, const char *filename)
     scriptObjPtr = Jim_NewStringObjNoAlloc(interp, buf, readlen);
     JimSetSourceInfo(interp, scriptObjPtr, Jim_NewStringObj(interp, filename, -1), 1);
     Jim_IncrRefCount(scriptObjPtr);
-
-    /* Now check the script for unmatched braces, etc. */
-    /* EvalFile changes context, so add a stack frame here */
-    if (Jim_GetScript(interp, scriptObjPtr, JIM_ERRMSG) == NULL) {
-        Jim_DecrRefCount(interp, scriptObjPtr);
-        return JIM_ERR;
-    }
 
     prevScriptObj = interp->currentScriptObj;
     interp->currentScriptObj = scriptObjPtr;
@@ -11653,7 +11648,7 @@ static int Jim_ForCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *argv
 
         /* Do it only if there aren't shared arguments */
         expr = JimGetExpression(interp, argv[2]);
-        incrScript = Jim_GetScript(interp, argv[3], JIM_NONE);
+        incrScript = JimGetScript(interp, argv[3]);
 
         /* Ensure proper lengths to start */
         if (incrScript == NULL || incrScript->len != 3 || !expr || expr->len != 3) {
@@ -12819,7 +12814,7 @@ static int Jim_DebugCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *ar
             Jim_WrongNumArgs(interp, 2, argv, "script");
             return JIM_ERR;
         }
-        script = Jim_GetScript(interp, argv[2], JIM_NONE);
+        script = JimGetScript(interp, argv[2]);
         if (script == NULL)
             return JIM_ERR;
         Jim_SetResultInt(interp, script->len);
@@ -14498,7 +14493,7 @@ static int Jim_InfoCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *arg
                 Jim_WrongNumArgs(interp, 2, argv, "");
                 return JIM_ERR;
             }
-            Jim_SetResult(interp, Jim_GetScript(interp, interp->currentScriptObj, JIM_NONE)->fileNameObj);
+            Jim_SetResult(interp, JimGetScript(interp, interp->currentScriptObj)->fileNameObj);
             break;
 
         case INFO_SOURCE:{
@@ -14523,7 +14518,7 @@ static int Jim_InfoCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *arg
                         line = argv[2]->internalRep.sourceValue.lineNumber;
                     }
                     else if (argv[2]->typePtr == &scriptObjType) {
-                        ScriptObj *script = Jim_GetScript(interp, argv[2], JIM_NONE);
+                        ScriptObj *script = JimGetScript(interp, argv[2]);
                         fileNameObj = script->fileNameObj;
                         line = script->firstline;
                     }