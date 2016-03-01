f591132d3a1b7214a2da1fc92aaf7a00425bb616
Evan Hunter <evan@ozhiker.com>
2016-03-01 09:18:05 +0000

    MinGW - Fix compilation error due to not having support for group or owner permission flags.

--- jim-aio.c
+++ jim-aio.c
@@ -1800,7 +1800,12 @@ int Jim_MakeTempFile(Jim_Interp *interp, const char *template)
         filenameObj = Jim_NewStringObj(interp, template, -1);
     }
 
+#if !defined(__MINGW32__)
     mask = umask(S_IXUSR | S_IRWXG | S_IRWXO);
+#else
+    /* MinGW does not have group/owner permissions */
+    mask = umask(S_IXUSR);
+#endif
 
     /* Update the template name directly with the filename */
     fd = mkstemp(filenameObj->bytes);
