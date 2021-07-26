67a16670f8448b73c15f5dc7c0c2101dda8af908
Steve Bennett <steveb@workware.net.au>
2021-07-26 19:26:50 +1000

    info tainted: check arg count
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- jim.c
+++ jim.c
@@ -15871,6 +15871,10 @@ static int Jim_InfoCoreCommand(Jim_Interp *interp, int argc, Jim_Obj *const *arg
         }
 
         case INFO_TAINTED:
+            if (argc != 3) {
+                Jim_WrongNumArgs(interp, 2, argv, "value");
+                return JIM_ERR;
+            }
             Jim_SetResultBool(interp, argv[2]->taint != 0);
             return JIM_OK;
         case INFO_CHANNELS:
