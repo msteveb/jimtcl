ea285785b27eafba908630c774404c28e01b6de9
Steve Bennett <steveb@workware.net.au>
2023-04-19 14:39:10 +1000

    regexp: fix check for termination in [[:class:]]
    Fixes #259
    
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- jimregexp.c
+++ jimregexp.c
@@ -792,6 +792,10 @@ static int regatom(regex_t *preg, int *flagp)
 					for (cc = 0; cc < CC_NUM; cc++) {
 						n = strlen(character_class[cc]);
 						if (strncmp(pattern, character_class[cc], n) == 0) {
+							if (pattern[n] != ']') {
+								preg->err = REG_ERR_UNMATCHED_BRACKET;
+								return 0;
+							}
 							/* Found a character class */
 							pattern += n + 1;
 							break;
