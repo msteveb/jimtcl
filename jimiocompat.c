48821870a1fcc876cd16973d37da0eb641c479cf
Jan Matyas <jan.matyas@codasip.com>
2026-05-22 16:06:32 +0200

    jimiocompat: Avoid unused variable compiler warnings
    In the *NIX implementation of Jim_MakeTempFile(), the variable
    "mask" becomes unused if HAVE_UMASK is not present, causing
    -Wunused-variable compiler warnings (if enabled).
    
    Make a trivial fix to address this.

--- jimiocompat.c
+++ jimiocompat.c
@@ -194,7 +194,9 @@ int Jim_OpenForRead(const char *filename)
 int Jim_MakeTempFile(Jim_Interp *interp, const char *filename_template, int unlink_file)
 {
     int fd;
+#ifdef HAVE_UMASK
     mode_t mask;
+#endif
     Jim_Obj *filenameObj;
 
     if (filename_template == NULL) {
